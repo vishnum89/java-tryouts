53a2fa4f649dee7cd1b45785b61a09627bd5aa22
unknown <vishnum89@gmail.com>
2020-10-28 16:22:53 +0530

    Path Variable and debug logs display

--- restful-werb-services/src/main/java/com/in28mins/rest/webservices/restfulwerbservices/HelloWorldController.java
+++ restful-werb-services/src/main/java/com/in28mins/rest/webservices/restfulwerbservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28mins.rest.webservices.restfulwerbservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping(path = "/helloWorldBean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("HelloWorld");
+    }
+
+    @GetMapping(path = "/helloWorldBean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean("HelloWorld " + name);
     }
 }
