235968cbbd6f58a0454f49d730c5abaa6b5fb620
unknown <vishnum89@gmail.com>
2020-10-28 15:12:22 +0530

    First Commit

---
+++ restful-werb-services/src/main/java/com/in28mins/rest/webservices/restfulwerbservices/RestfulWerbServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28mins.rest.webservices.restfulwerbservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWerbServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWerbServicesApplication.class, args);
+	}
+
+}
---
+++ restful-werb-services/src/test/java/com/in28mins/rest/webservices/restfulwerbservices/RestfulWerbServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28mins.rest.webservices.restfulwerbservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWerbServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
